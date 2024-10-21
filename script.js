557c995b40522bcd3f605660ea40de2370a77adf
Rishijangir566 <rishijangirjr@gmail.com>
2024-10-21 14:05:34 +0530

    generate api with question

--- script.js
+++ script.js
@@ -15,31 +15,33 @@ showQuesAns.innerHTML="Loading Data"
   const actualURL=`${baseURL}?${getQueryString(dataplayer)}`
 
 //    const actualURL = `${baseURL}?amount=${dataplayer.amount}&category=${dataplayer.category}&difficulty=${dataplayer.difficulty}&type=${dataplayer.type}`
-    //   console.log(actualURL)
-   fetchData(actualURL)
+      console.log(actualURL)
+   fetchData(actualURL)  // fetchdata is function
 }
-
-
-function getQueryString(data){
+               
+function getQueryString(data){   // datapalyer is convert is data 
    let querystring="";
-   for(let x in data){
-    querystring+=querystring.length===0 ?`${x}=${data[x]}`:`&${x}=${data[x]}`
+   for(let x in data){         // data is a object then object loop is for in  
+    querystring+= querystring.length===0 ?`${x}=${data[x]}`:`&${x}=${data[x]}`
+    // if querystring===0 hai to ${x}=${data.x} and (nhi to) &${x}=${data.x}
+                     // data is key value so x is key and data.x is value
    }
    return querystring
 }
-
+     // fetchdata (acturalURL) is convert (url)  
 function fetchData (url){
     fetch(url)
-    .then((response)=> response.json())
-    .then((result)=> showData(result.results))
+    .then((response)=> response.json()) // url in object formet 
+    .then((result)=> showData(result.results)) // url convert in tree array of object format json () read easy 
 }
 
 
- function showData(data){
+       // url array of object format read easily to convert to data 
+ function showData(data){  
     showQuesAns.innerHTML=""
-    const newData = data.map((obj)=>{
-        obj.incorrect_answers.push(obj.correct_answer)
-        return obj
+                    // data in 30 item to har ek pe (.map())
+    const newData = data.map((obj)=>{ obj.incorrect_answers.push(obj.correct_answer)
+        return obj   // har ek obj ki incorct answer me correct answer push kr do
     })
   
     let quesAns = document.createElement("div")
@@ -50,16 +52,19 @@ function fetchData (url){
         const randomoptions =randomize(obj.incorrect_answers);
 
         const question= document.createElement("p")
-        question.classList.add("question")
-        question.innerText=obj.question;
+        question.classList.add("questions")
+        question.innerText=`Ques.  ${obj.question}`;
+        
 
-        const options = document.createElement("div")
-        options.classList.add("options")
+        const optionDIv = document.createElement("div")
+        const options = document.createElement("ul")
+        optionDIv.classList.add("optiondiv")
         for (let i=0; i<4; i++){
-            const option = document.createElement("p")
+            const option = document.createElement("li")
             option.classList.add("option")
             option.innerText=randomoptions[i]
             options.append(option)
+            optionDIv.append(options)
         }
       
      quesAns.append(question,options)
@@ -71,9 +76,6 @@ function fetchData (url){
 
  }
 
-   
-
-
  function randomize(arr){
       const randomvalue =[];
       for (let i=0 ; i<arr.length;i++){
@@ -83,3 +85,11 @@ function fetchData (url){
       }
       return randomvalue
  }
+
+
+//  let a=10;
+//   let b=5;
+
+//   a=a+b // 15
+//   b=a-b //10
+//   a=a-b //5
